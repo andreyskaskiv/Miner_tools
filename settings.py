8b5f1d86ce6c6938eb1d99685e4f0fa925f99316
andrey <andreyskaskiv.py@gmail.com>
2023-12-15 19:49:56 +0200

    profit switch

---
+++ settings.py
@@ -0,0 +1,11 @@
+MAX_LOAD = 80
+MAX_TEMP = 60
+ELECTRICITY_COST = 0.07
+
+MINER_PATHS = {
+    "Pyrin": r"G:\\Miner\\1.81\\Pyrin.bat",
+    "Karlsen": r"G:\\Miner\\1.81\\Karlsen.bat",
+    "Alephium":  r"G:\\Miner\\1.81\\Alephium.bat",
+}
+
+
